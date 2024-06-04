1d67f2240f26813e0090220d7401ae6a15816468
Potentially-Human <PotentiallyHuman@proton.me>
2024-06-03 21:51:50 -0400

    modified:   index.html 	renamed:    assets/close.png -> static/assets/close.png 	renamed:    assets/menu.png -> static/assets/menu.png 	modified:   static/script.js 	new file:   static/search.js 	modified:   static/style.css

--- static/script.js
+++ static/script.js
@@ -1,3 +1,28 @@
-document.querySelector(".navbar").addEventListener("click", () => {
-    
+window.addEventListener("load", (event) => {
+    const sidenav = document.querySelector(".sidenav");
+    const overlay =  document.querySelector(".overlay");
+    document.querySelector(".navbar").addEventListener("click", (event) => {     
+        sidenav.style.display = "block";
+        overlay.style.display = "block";
+        sidenav.animate([
+            {transform: "translateX(-200px)"}, 
+            {transform: "translateX(0px)"}, 
+        ], {
+            duration: 300,
+            iterations: 1,
+        })
+    });
+
+    document.querySelector(".close-sidebar").addEventListener("click", (event) => {
+        sidenav.animate([
+            {transform: "translateX(0px)"}, 
+            {transform: "translateX(-200px)"}, 
+        ], {
+            duration: 300,
+            iterations: 1,
+        })
+        sidenav.style.display = "none";
+        overlay.style.display = "none";
+        
+    });
 })
